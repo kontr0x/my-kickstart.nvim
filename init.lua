7effdf850af2685fdd26beac5f1ae4aaa0713eb7
Bayram Kazik <48856944+bayramkzk@users.noreply.github.com>
2024-08-25 00:31:43 +0300

    Include visual mode in LSP code action keymap (#1060) (#1064)

--- init.lua
+++ init.lua
@@ -230,6 +230,7 @@ require('lazy').setup({
       -- Document existing key chains
       spec = {
         { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>c_', hidden = true },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
